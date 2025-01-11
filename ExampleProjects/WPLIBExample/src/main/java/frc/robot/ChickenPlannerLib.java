faa0fa4e2b4aaf39b95db92e73a3973e70d7b6df
Emilio <emiliomoreno03096@gmail.com>
2025-01-11 11:13:13 -0600

    Added new distribution

--- ExampleProjects/WPLIBExample/src/main/java/frc/robot/ChickenPlannerLib.java
+++ ExampleProjects/WPLIBExample/src/main/java/frc/robot/ChickenPlannerLib.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import java.io.File;
@@ -387,10 +383,4 @@ public class ChickenPlannerLib {
           return "(" + x + ", " + y + ")";
       }
   }
-
-public static SequentialCommandGroup getFollowChickenPathCommand(String string, TrajectoryConfig trajectoryConfig,
-        RamseteController m_ramsete, Drivetrain drivetrain, BiConsumer<Double, Double> biConsumer) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getFollowChickenPathCommand'");
-}
 }
